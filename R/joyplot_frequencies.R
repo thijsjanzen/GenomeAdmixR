3617402223339edcc766c7671f2e40883dcc3b50
Thijs Janzen <thijsjanzen@gmail.com>
2020-09-13 21:08:39 +0200

    Update joyplot_frequencies.R

--- R/joyplot_frequencies.R
+++ R/joyplot_frequencies.R
@@ -56,21 +56,21 @@ plot_joyplot_frequencies <- function(frequencies,
   vz$ancestor <- as.factor(vz$ancestor)
 
   if (picked_ancestor == "ALL") {
-    p1 <- ggplot2::ggplot(vz, ggplot2::aes(x = vz$location,
-                                           y = as.factor(vz$time),
-                                           height = vz$frequency,
-                                           fill = vz$ancestor)) +
+    p1 <- ggplot2::ggplot(vz, ggplot2::aes(x = location,
+                                           y = as.factor(time),
+                                           height = frequency,
+                                           fill = ancestor)) +
       ggridges::geom_ridgeline(scale = 1.3)
   } else {
     vy <- subset(vz, vz$ancestor == picked_ancestor)
-    p1 <- ggplot2::ggplot(vy, ggplot2::aes(x = vy$location,
-                                           y = as.factor(vy$time),
-                                           height = vy$frequency)) +
+    p1 <- ggplot2::ggplot(vy, ggplot2::aes(x = location,
+                                           y = as.factor(time),
+                                           height = frequency)) +
       ggridges::geom_ridgeline(scale = 1.3,
                                fill = "lightblue")
   }
   p1 <- p1 +
     ggplot2::labs(fill = "Ancestor")  +
     ggplot2::ylab("Time") +
-    ggplot2::xlab("Location")
+    ggplot2::xlab("Location (Morgan)")
 }
