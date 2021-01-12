f0e6151e1005d3facf68b9904f03d64b0f9a446a
Thijs Janzen <thijsjanzen@gmail.com>
2021-01-12 21:44:52 +0100

    Update test-LD.R

--- tests/testthat/test-LD.R
+++ tests/testthat/test-LD.R
@@ -3,7 +3,7 @@ context("LD stats")
 test_that("calculate_average_LD", {
   pop_size <- 100
   number_of_founders <- 2
-  run_time <- 1
+  run_time <- 1000
   morgan <- 1
   write_to_file <- FALSE
 
@@ -13,7 +13,7 @@ test_that("calculate_average_LD", {
                              morgan = morgan)$population
 
   number_of_markers <- 2
-  markers <- c(0.25, 0.26)
+  markers <- c(0.01, 0.99)
 
   all_loci <- matrix(nrow = length(pop1),
                      ncol = 2 * number_of_markers,
@@ -36,7 +36,46 @@ test_that("calculate_average_LD", {
   g2 <- all_loci[, 3:4]
 
   vv <- calculate_average_ld(g1, g2)
-  testthat::expect_gte(vv$LD, 0.5)
+  testthat::expect_equal(vv$LD, 0.0)
+
+
+  pop_size <- 1000
+  number_of_founders <- 2
+  run_time <- 1
+  morgan <- 1
+  write_to_file <- FALSE
+
+  pop1 <- simulate_admixture(pop_size = pop_size,
+                             number_of_founders = number_of_founders,
+                             total_runtime = run_time,
+                             morgan = morgan)$population
+
+  number_of_markers <- 2
+  markers <- c(0.5, 0.5+1e-4)
+
+  all_loci <- matrix(nrow = length(pop1),
+                     ncol = 2 * number_of_markers,
+                     0)
+
+  for (x in seq_along(markers)) {
+    focal_marker <- markers[x]
+    for (i in seq_along(pop1)) {
+      allele_1 <- 1 + findtype(pop1[[i]]$chromosome1, focal_marker)
+      allele_2 <- 1 + findtype(pop1[[i]]$chromosome2, focal_marker)
+
+      index <- (x - 1) * 2 + 1
+
+      all_loci[i, index]     <- as.numeric(allele_1)
+      all_loci[i, index + 1] <- as.numeric(allele_2)
+    }
+  }
+
+  g1 <- all_loci[, 1:2]
+  g2 <- all_loci[, 3:4]
+
+  vv <- calculate_average_ld(g1, g2)
+  testthat::expect_equal(vv$LD, 1.0)
+
 })
 
 test_that("calculate_LD_matrix", {
