29d83a9512b422206fddb10c3f87f19478de2043
Thijs Janzen <thijsjanzen@gmail.com>
2021-01-14 08:09:21 +0100

    junctions

--- tests/testthat/test-simulate_admixture.R
+++ tests/testthat/test-simulate_admixture.R
@@ -1,42 +1,16 @@
 context("simulate_Admixture")
 
 
-test_that("simulate admixture use, markers", {
-  pop <- simulate_admixture(pop_size = 1000,
-                            number_of_founders = 2,
-                            total_runtime = 3,
-                            markers = seq(0, 1, length.out = 1000),
-                            morgan = 1)
 
-  a <- subset(pop$final_frequency, pop$final_frequency$location < 0.5)
-  b <-  subset(pop$final_frequency, pop$final_frequency$location > 0.5)
-  testthat::expect_equal(dim(a), dim(b))
+test_that("simulate admixture use, junctions", {
+  markers <- 0.5
+  vx <- simulate_admixture(pop_size = 100,
+                           number_of_founders = 2,
+                           total_runtime = 100,
+                           markers = markers,
+                           track_junctions = TRUE)
 
-  testthat::expect_silent(
-    GenomeAdmixR::plot_difference_frequencies(pop)
-  )
-
-
-  pop <- simulate_admixture(pop_size = 1000,
-                            number_of_founders = 2,
-                            total_runtime = 3,
-                            markers = seq(0, 3, length.out = 1000),
-                            morgan = 3)
-
-  a <- subset(pop$final_frequency, pop$final_frequency$location < 1.5)
-  b <-  subset(pop$final_frequency, pop$final_frequency$location > 1.5)
-  testthat::expect_equal(dim(a), dim(b))
-
-  GenomeAdmixR::plot_difference_frequencies(pop)
-
-  pop <- simulate_admixture(pop_size = 1000,
-                            number_of_founders = 2,
-                            total_runtime = 3,
-                            markers = seq(0, 5, length.out = 1000),
-                            morgan = 5)
-
-  a <- subset(pop$final_frequency, pop$final_frequency$location < 2.5)
-  b <-  subset(pop$final_frequency, pop$final_frequency$location > 2.5)
-  testthat::expect_equal(dim(a), dim(b))
-  GenomeAdmixR::plot_difference_frequencies(pop)
+  num_j <- length(vx$junctions)
+  testthat::expect_gt(num_j, 0)
+  testthat::expect_equal(num_j, 100)
 })
