00766ecb7db6fd0548430c122fd7515516b65b56
Thijs Janzen <thijsjanzen@gmail.com>
2021-01-29 12:51:30 +0100

    Update test-selection.R

--- tests/testthat/test-selection.R
+++ tests/testthat/test-selection.R
@@ -17,9 +17,6 @@ test_that("select population two_alleles", {
   testthat::expect_true(verify_population(selected_pop$population))
 
   vv <- calculate_marker_frequency(selected_pop$population, 0.05)
-  a <- which.max(vv$frequency)
-  b <- vv$ancestor[a]
-  testthat::expect_equal(as.numeric(b), 0)
 
 
   number_of_founders <- 10
