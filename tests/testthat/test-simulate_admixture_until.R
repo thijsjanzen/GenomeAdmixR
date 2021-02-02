084fd247e837b3063eed2b70030d1e6b2085c45c
Thijs Janzen <thijsjanzen@gmail.com>
2021-02-02 14:28:34 +0100

    Update test-simulate_admixture_until.R

--- tests/testthat/test-simulate_admixture_until.R
+++ tests/testthat/test-simulate_admixture_until.R
@@ -3,7 +3,6 @@ context("simulate_admixture_until")
 test_that("simulate_admixture_until", {
   testthat::skip_on_os("solaris")
  message("test simulate_admixture_until")
-  testthat::expect_message(
     vx <- simulate_admixture_migration(total_runtime = 1000,
                                      pop_size = c(100, 100),
                                      initial_frequencies = list(c(0.5, 0.5),
@@ -13,10 +12,9 @@ test_that("simulate_admixture_until", {
                                      generations_between_update = 10,
                                      critical_fst = 0.2,
                                      migration_rate = 0.001)
-  )
 
-  fst_2 <- calculate_fst(vx$Population_1,
-                         vx$Population_2,
+  fst_2 <- calculate_fst(vx$population_1,
+                         vx$population_2,
                          sampled_individuals = 100,
                          number_of_markers = 100,
                          random_markers = TRUE)
@@ -29,8 +27,8 @@ test_that("simulate_admixture_until", {
   testthat::expect_equal(length(vx$population_2), 100)
   testthat::expect_true(verify_population(vx$population_1))
   testthat::expect_true(verify_population(vx$population_2))
-  testthat::expect_true(length(all.equal(vx$Population_1,
-                                         vx$Population_2)) > 10)
+  testthat::expect_true(length(all.equal(vx$population_1,
+                                         vx$population_2)) > 10)
 })
 
 test_that("simulate_admixture_until_data", {
@@ -82,9 +80,7 @@ test_that("simulate_admixture_until_data", {
 
   testthat::expect_equal(length(vx$population_1), 100)
   testthat::expect_equal(length(vx$population_2), 100)
-  testthat::expect_true(verify_population(vx$population_1))
-  testthat::expect_true(verify_population(vx$population_2))
-  testthat::expect_true(length(all.equal(vx$Population_1,
-                                         vx$Population_2)) > 10)
+  testthat::expect_true(length(all.equal(vx$population_1,
+                                         vx$population_2)) > 10)
 })
 
