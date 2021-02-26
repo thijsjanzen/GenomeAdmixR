b724dd34c791f9872350103d77f643dd13acad35
Thijs Janzen <thijsjanzen@gmail.com>
2021-02-26 09:41:41 +0100

    update data entry

--- R/data.R
+++ R/data.R
@@ -1,7 +1,8 @@
 #' A subset of sequencing data from the Drosophila Genetics Reference Panel
 #' @description This data set contains sequences from the 3R chromosome.
-#' Included are 4603 SNPs, sequenced across 410 isofemale lines. Sequences were
-#' downloaded from <http://dgrp2.gnets.ncsu.edu/data.html>.
+#' Included are 4603 SNPs with at least 0.05 minor allele frequency,  sequenced
+#' across 410 isofemale lines. Sequences were downloaded from
+#' <http://dgrp2.gnets.ncsu.edu/data.html>.
 #' @references Mackay, T., Richards, S., Stone, E. et al. The Drosophila
 #' melanogaster Genetic Reference Panel. Nature 482, 173–178 (2012).
 #' <https://doi.org/10.1038/nature10811>
